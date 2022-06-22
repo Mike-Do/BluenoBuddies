34751db090e13d2617e62fc4a899c83cdf584d66
Mike-Do <agentdonoso@gmail.com>
2022-06-22 11:51:47 -0400

    feat Create dummy data for rooms

--- base/views.py
+++ base/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Dummy data for rooms
+rooms = [
+    {'id':1, 'name':'Lets learn Python!'}
+    {'id':2, 'name':'Design with me!'}
+    {'id':3, 'name':'Frontend developers'}
+]
+
 def home(request):
     """Home Page View"""
     return render(request, 'home.html')
