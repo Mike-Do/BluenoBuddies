650943bfc8445028bae279fea61c0ce3cd06c834
Mike-Do <agentdonoso@gmail.com>
2022-06-22 12:40:08 -0400

    feat Configure admin panel and add Rooms

--- base/models.py
+++ base/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Room(models.Model):
+    #host = 
+    #topic =
+    name = models.CharField(max_length=200)
+    # descriptions can be empty
+    description = models.TextField(null=True, blank=True)
+    # participants =
+    # Updated and Created automatically set to current time
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.name)
+
+    
+
+
+
