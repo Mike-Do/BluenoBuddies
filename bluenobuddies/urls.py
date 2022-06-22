6d7f24c5a5983720ee9047ce5724cadcf596e304
Mike-Do <agentdonoso@gmail.com>
2022-06-22 11:29:25 -0400

    feat Create Home Page

--- bluenobuddies/urls.py
+++ bluenobuddies/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+def home(request):
+    """Home Page URL"""
+    return HttpResponse('Home page')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home),
 ]
