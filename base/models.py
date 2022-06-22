51978523ee11b7078ef61cb254fdacef10925b5a
Mike-Do <agentdonoso@gmail.com>
2022-06-22 13:00:53 -0400

    feat Migrate new models to DB

--- base/models.py
+++ base/models.py
@@ -9,9 +9,9 @@ class Topic(models.Model):
         return self.name
 
 class Room(models.Model):
-    #host = 
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     # Set topic of room to Null if topic is deleted
-    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL)
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
     # descriptions can be empty
     description = models.TextField(null=True, blank=True)
