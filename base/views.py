bb0c9933365bbba4690c98ab02de0b464b7b1edd
Mike-Do <agentdonoso@gmail.com>
2022-06-22 11:39:33 -0400

    feat Create HTML files for Home and Room pages

--- base/views.py
+++ base/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    """Home Page URL"""
+    return HttpResponse('Home page')
+
+def room(request):
+    return HttpResponse('ROOM')
