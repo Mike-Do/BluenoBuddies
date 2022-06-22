5c9b88ad1edc393b4a9c6dca4e914f1715d7ddbc
Mike-Do <agentdonoso@gmail.com>
2022-06-22 12:58:59 -0400

    feat Create a Topic model for Rooms

--- base/models.py
+++ base/models.py
@@ -1,11 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Topic(models.Model):
+    """Parent model to Rooms"""
+    name = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.name
 
 class Room(models.Model):
     #host = 
-    #topic =
+    # Set topic of room to Null if topic is deleted
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL)
     name = models.CharField(max_length=200)
     # descriptions can be empty
     description = models.TextField(null=True, blank=True)
