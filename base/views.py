b242e5528eec630642c71e87ebd6f8c42423af02
Mike-Do <agentdonoso@gmail.com>
2022-06-22 11:58:50 -0400

    feat Reorganize templating to base app

--- base/views.py
+++ base/views.py
@@ -2,15 +2,17 @@ from django.shortcuts import render
 
 # Dummy data for rooms
 rooms = [
-    {'id':1, 'name':'Lets learn Python!'}
-    {'id':2, 'name':'Design with me!'}
-    {'id':3, 'name':'Frontend developers'}
+    {'id':1, 'name': 'Lets learn Python!'},
+    {'id':2, 'name': 'Design with me!'},
+    {'id':3, 'name': 'Frontend developers'},
 ]
 
 def home(request):
     """Home Page View"""
-    return render(request, 'home.html')
+    # context dict stores data sent to templates
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
 
 def room(request):
     """Home Page View"""
-    return render(request, 'room.html')
+    return render(request, 'base/room.html')
