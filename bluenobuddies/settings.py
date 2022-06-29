d84ff2bf35b4aafd9d3d532ad2f244ad077e4203
Mike-Do <agentdonoso@gmail.com>
2022-06-29 13:38:57 -0400

    feat Install Django CORS headers and allow for APIrequests from any source

--- bluenobuddies/settings.py
+++ bluenobuddies/settings.py
@@ -41,12 +41,16 @@ INSTALLED_APPS = [
     # connect custom apps
     'base.apps.BaseConfig',
 
-    # connect Django REST framework
+    # connect Django REST framework and CORS headers
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+
+    "corsheaders.middleware.CorsMiddleware",
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -139,3 +143,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
